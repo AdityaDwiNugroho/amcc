b832518c6dc8f27306d3a9b70677361cfad0310f
AdityaDwiNugroho <veldora5000@gmail.com>
2026-01-03 11:22:58 +0800

    Add variable types

---
+++ _4variable.php
@@ -0,0 +1,14 @@
+<?php
+
+$string = "Ini string"; // string
+$integere = 42; // integer
+$float = 3.14; // float
+$boolean = true; // boolean
+$array = array("apel", "pisang", "jeruk"); // array
+
+
+echo gettype($string) . "\n";   // Output: string
+echo gettype($integere) . "\n";  // Output: integer
+echo gettype($float) . "\n";    // Output: double
+echo gettype($boolean) . "\n";  // Output: boolean
+echo gettype($array) . "\n";    // Output: array

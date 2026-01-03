a12d81b17f14ba76be525972a237853115342720
AdityaDwiNugroho <veldora5000@gmail.com>
2026-01-03 11:23:32 +0800

    Add statement example

---
+++ _2statement.php
@@ -0,0 +1,7 @@
+<?php
+
+$nama = "Yurtzy";
+$umur = 18;
+$pekerjaan = "Mahasiswa";
+
+echo "Nama saya\t" . $nama . "\tumur saya\t " . $umur . "\ttahun, dan saya adalah seorang\t" . $pekerjaan . ".";
